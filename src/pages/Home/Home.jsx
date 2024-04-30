eabf86121399949485ea4c31bda780580e871dff
Sangma83 <poushalisangma13@gmail.com>
2024-04-30 23:02:36 +0600

    eye catching footer added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import HomeBelow from "../HomeBelow/HomeBelow";
+import Footer from "../shared/Footer/Footer";
 import Header from "../shared/Header/Header";
 import Navbar from "../shared/Navbar/Navbar";
 
@@ -9,6 +10,7 @@ const Home = () => {
       <Header></Header>
       <HomeBelow></HomeBelow>
       <h2 className="text-4xl font-rambla">this is home page</h2>
+      <Footer></Footer>
     </div>
   );
 };
--- src/pages/shared/Navbar/Navbar.jsx
+++ src/pages/shared/Navbar/Navbar.jsx
@@ -24,16 +24,17 @@ const Navbar = () => {
     const navLinks = <>
        <li><NavLink to='/'>Home</NavLink></li>
        <li><NavLink to='/allCraftitem'>Art & Craft Item</NavLink></li>
-       <li><NavLink to='/contact'>Contact Us</NavLink></li>
-       {/* <li><NavLink to='/login'>Login</NavLink></li> */}
-       {/* <li><NavLink to='/register'>Register</NavLink></li> */}
-      {
+       {
         user && <>
          <li><NavLink to='/addItem'>Add Craft Item</NavLink></li>
          <li><NavLink to='/myList'>My List</NavLink></li>
          {/* <li><NavLink to='/update'>Update Profile</NavLink></li> */}
         </>
       }
+       <li><NavLink to='/contact'>Contact Us</NavLink></li>
+       {/* <li><NavLink to='/login'>Login</NavLink></li> */}
+       {/* <li><NavLink to='/register'>Register</NavLink></li> */}
+      
     
     </>
 
