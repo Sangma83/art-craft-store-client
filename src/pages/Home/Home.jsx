e227ef1bc563da294f507d36e41f40488e31e470
Sangma83 <poushalisangma13@gmail.com>
2024-04-30 23:41:10 +0600

    footer newsletter added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import HomeBelow from "../HomeBelow/HomeBelow";
+import NewsLetter from "../NewsLetter/NewsLetter";
 import Footer from "../shared/Footer/Footer";
 import Header from "../shared/Header/Header";
 import Navbar from "../shared/Navbar/Navbar";
@@ -10,6 +11,8 @@ const Home = () => {
       <Header></Header>
       <HomeBelow></HomeBelow>
       <h2 className="text-4xl font-rambla">this is home page</h2>
+      <div className="divider divide-orange-900 "></div>
+      <NewsLetter></NewsLetter>
       <Footer></Footer>
     </div>
   );
---
+++ src/pages/NewsLetter/NewsLetter.jsx
@@ -0,0 +1,20 @@
+
+
+const NewsLetter = () => {
+    return (
+        <div className="my-10">
+            <form>
+    <h6 className="font-bold text-2xl text-center text-black">SIGN UP FOR THE NEWSLETTER</h6> 
+    <div className="divider divide-[#EB984E] w-1/3 ml-96"></div>
+    <fieldset className="form-control w-80 ml-96"> 
+      <div className="join">
+        <input type="text" placeholder="email address" className="input input-bordered join-item px-16 border-orange-800" /> 
+        <button className="btn bg-black text-white join-item">SUBSCRIBE</button>
+      </div>
+    </fieldset>
+  </form>
+        </div>
+    );
+};
+
+export default NewsLetter;
